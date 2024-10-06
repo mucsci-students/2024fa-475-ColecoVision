76075e7a11c8fe7846d47aba0f25c23ed9190591
Tyler Strohl <twstrohl@millersville.edu>
2024-10-05 23:09:44 -0400

    Stamina now decreases/increases when using the arrow keys to move. Started the "Dialogue" C# Script.

--- Game/Assets/Prefabs/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Game/Assets/Prefabs/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -90,7 +90,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             //Stamina/Sprint Duration
             if (!m_IsWalking)
             {
-                if (SprintDuration > 0 && ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.S)) || (Input.GetKey(KeyCode.D))))
+                if (SprintDuration > 0 && ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.S)) || (Input.GetKey(KeyCode.D)) || (Input.GetKey(KeyCode.UpArrow)) || (Input.GetKey(KeyCode.DownArrow)) || (Input.GetKey(KeyCode.LeftArrow)) || (Input.GetKey(KeyCode.RightArrow))))
                 {
                     SprintDuration--;
                 }
