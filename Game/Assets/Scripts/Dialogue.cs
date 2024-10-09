1ec7df294666a5479193b40acb074cc85692e04e
Chayse Altland <chayse@Chayses-MacBook-Air.local>
2024-10-09 14:45:40 -0400

    Completed the dialogue script, should work for all tiers.

--- Game/Assets/Scripts/Dialogue.cs
+++ Game/Assets/Scripts/Dialogue.cs
@@ -1,110 +1,159 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class Dialogue : MonoBehaviour
 {
+    public TextMeshProUGUI textComponent;
+    public TextMeshProUGUI nameComponent;
+    public float textSpeed;
+    public GameObject player;
 
-    //Dialogue/text will be stored as an array of strings (was going to do vector/list).
-    //Will use a for loop to go through the appropriate indexes.
-    //Print the text/string out like how we did in the book's games (amazing racer, chaos ball)
-    //Will need to add some lines or a method or something to space out the prints.
-
-
+    private int index;
+    private string[] lines;
+    private string[] names;
     // Start is called before the first frame update
     void Start()
     {
-        //for testing purposes.
-        print(Script());
+        textComponent.text = string.Empty;
+        zeroTierScript();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) {
+            if (textComponent.text == lines[index]) {
+                NextLine();
+            }
+            else {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
     }
+    void StartDialogue() {
+        player.GetComponent<FirstPersonController>().enabled = false;
+        Time.timeScale = 0;
+         Debug.Log("Starting dialogue. Line: " + lines[index]);
+        index = 0;
 
-    //List acts as vector would in c++ or arraylist in java ??
-    string[] Script()
-    {
-        //List<string> theLines = new List<string>();
-        string[] theLines = new string[50];
-        
-        //***********************************
-        //this loop is a test.
-        //for (int i = 0; i <= 10; i++)
-        //{
-            //theLines.Add(i.ToString());
-            //print(theLines[i]);
-        //}
-        //***********************************
-
-        //TheLines for Tier 0.
-        theLines[0] = "I see you are the latest competitor";
-        theLines[1] = "I do not remember meeting you before, so I assume you are new.";
-        theLines[2] = "In that case, I will go easy on you. Not too sure if my assistants will however";
-        theLines[3] = "What do you mean by “assistants!?” We’re all on the same team with the same power!";
-        theLines[4] = "Yeah, come on Jav, you cannot have everything to yourself. Do I need to remind you what happened the last time we ordered pizza? You started shouting that everyone needed to give their slices over to you!";
-        theLines[5] = "Ok fine. So, do one of you two want to explain the game rules to our new challenger?";
-        theLines[6] = "Sure, I’ll do it. Your goal in this game is to make it to the end of this dungeon. Do it by whatever means necessary. As long as you do not die, you will get a high score.";
-        theLines[7] = "If you die, it is game over. I believe that it is enough information. It is pretty simple, right?";
-        theLines[8] = "Agreed.";
-        theLines[9] = "Ok, good luck out there. Well, at least as much as you can have.";
-
-        //will have to add an extra line in the loop or something to not make this all spam out at once, and also to see it on-screen.
-        for (int tZero = 0; tZero <= 9; tZero++)
-        {
-            //theLines.Add(theLines[tZero]);
-            print(theLines[tZero]);
-        }
-
-        //TheLines for Tier 1.
-        theLines[10] = "I’m sure you’re confused why you’re back in this room.";
-        theLines[11] = "It turns out we lied a little bit. OOPS!";
-        theLines[12] = "Well Jav always lies and changes his mind. Yeah, so you will have to clear through this dungeon again. Except this time, you will make it more difficult for yourself.";
-        theLines[13] = "It is not to be mean; it is just how the game works. But don’t worry: it’s not all bad news!";
-        theLines[14] = "You are going to choose to add a new enemy, trap, or room to this dungeon. But this will give you more points to your high score. Everyone likes an even higher high score, right? It's cool!";
-        theLines[15] = "Ok, add your new obstacle, and clear the dungeon again! Wishing you a merry dungeon run and a happy new high score!";
-
-        //see tZero loop.
-        for (int tOne = 10; tOne <= 15; tOne++)
-        {
-            print(theLines[tOne]);
-        }
-
-        //TheLines for Tier 2.
-        theLines[16] = "How did I get into this mess?";
-        theLines[17] = "That’s what I wonder too. I feel tricked!";
-        theLines[18] = "I heard there is a way for each player to escape their dungeon.";
-        theLines[19] = "Do you know what it is?";
-        theLines[20] = "Shhhh.. I heard one of the emperors coming towards us!";
-
-        //see tZero loop.
-        for (int tTwo = 16; tTwo <= 20; tTwo++)
-        {
-            print(theLines[tTwo]);
-        }
-
-        //TheLines for Tier 3.
-        theLines[21] = "Ah yes, we meet again.";
-        theLines[22] = "That was supposed to be my line!";
-        theLines[23] = "C said you can’t have everything to yourself, Tyler.";
-        theLines[24] = "Which one of you two said that!?";
-        theLines[25] = "Anyways challenger, We came here to tell you just how great of a job you are doing, even though you are stuck here basically forever.";
-        theLines[26] = "Well, there may be a way out for you. Not that I would want you to find that out, because I enjoy watching you play this game.";
-        theLines[27] = "Only Gary has ever escaped successfully. Most players struggle to escape.";
-        theLines[28] = "Does the player even know who Gary is?";
-        theLines[29] = "I have said too much. I won’t be in trouble, will I?";
-        theLines[30] = "Don’t be upset. I’m just happy the player hasn’t bombed the place yet. Wait I wasn’t supposed to say the yet part. STOP TALKING!";
-
-        //see tZero loop.
-        for (int tThree = 21; tThree <= 30; tThree++)
-        {
-            print(theLines[tThree]);
-        }
-
-        //**********************************
-        return theLines;
+nameComponent.text = names[index];
+        StartCoroutine(TypeLine());
     }
-
+    IEnumerator TypeLine() {
+        Debug.Log("Typing line...");
+        // Type each character 1 by 1
+        foreach (char c in lines[index].ToCharArray()) {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+    void NextLine() {
+        index++;
+        if (index < lines.Length && !string.IsNullOrEmpty(lines[index])) {
+            textComponent.text = string.Empty;
+            nameComponent.text = string.Empty;
+            nameComponent.text = names[index];
+            StartCoroutine(TypeLine());
+        }
+        else {
+            gameObject.SetActive(false);
+            player.GetComponent<FirstPersonController>().enabled = true;
+            Time.timeScale = 1;
+        }
+    }
+    //tier 0 script when the player starts the game
+    void zeroTierScript() {
+        lines = new string[10];
+        lines[0] = "I see you are the latest competitor";
+        lines[1] = "I do not remember meeting you before, so I assume you are new.";
+        lines[2] = "In that case, I will go easy on you. Not too sure if my assistants will however";
+        lines[3] = "What do you mean by assistants!? Weâ€™re all on the same team with the same power!";
+        lines[4] = "Yeah, come on Jav, you cannot have everything to yourself. Do I need to remind you what happened the last time we ordered pizza?";
+        lines[5] = "Ok fine. So, do one of you two want to explain the game rules to our new challenger?";
+        lines[6] = "Sure, Iâ€™ll do it. Your goal in this game is to make it to the end of this dungeon. Do it by whatever means necessary.";
+        lines[7] = "If you die, it is game over. I believe that it is enough information. It is pretty simple, right?";
+        lines[8] = "Agreed.";
+        lines[9] = "Ok, good luck out there. Well, at least as much as you can have.";
+//tier 0 names that correspond to their lines
+        names = new string[10];
+        names[0] = "Jav Ascript";
+        names[1] = "Jav Ascript";
+        names[2] = "Jav Ascript";
+        names[3] = "C. Plusplus";
+        names[4] = "C. Sharp";
+        names[5] = "Jav Ascript";
+        names[6] = "C. Sharp";
+        names[7] = "C. Plusplus";
+        names[8] = "Jav Ascript & C. Sharp";
+        names[9] = "Jav Ascript";
+        StartDialogue();
+    }
+    //tier one lines after the player complete the dungeon the first time
+    void firstTierScript() {
+        lines = new string[6];
+        lines[0] = "Iï¿½m sure youï¿½re confused why youï¿½re back in this room.";
+        lines[1] = "It turns out we lied a little bit. OOPS!";
+        lines[2] = "Well Jav always lies and changes his mind. Yeah, so you will have to clear through this dungeon again. Except this time, you will make it more difficult for yourself.";
+        lines[3] = "It is not to be mean; it is just how the game works. But donï¿½t worry: itï¿½s not all bad news!";
+        lines[4] = "You are going to choose to add a new enemy, trap, or room to this dungeon. But this will give you more points to your high score. Everyone likes an even higher high score, right? It's cool!";
+        lines[5] = "Ok, add your new obstacle, and clear the dungeon again! Wishing you a merry dungeon run and a happy new high score!";
+        
+        names = new string[6];
+        names[0] = "C. Sharp";
+        names[1] = "Jav Ascript";
+        names[2] = "C. Plusplus";
+        names[3] = "Jav Ascript";
+        names[4] = "C. Sharp";
+        names[5] = "Jav Ascript";
+        names[6] = "Jav Ascript";
+        StartDialogue();
+    }
+//tier 2 lines
+    void secondTierScripts() {
+        lines = new string[5];
+        lines[0] = "How did I get into this mess?";
+        lines[1] = "That's what I wonder too. I feel tricked!";
+        lines[2] = "I heard there is a way for each player to escape their dungeon.";
+        lines[3] = "Do you know what it is?";
+        lines[4] = "Shhhh.. I heard one of the emperors coming towards us!";
+        
+        names = new string[5];
+        names[0] = "Competitor 1";
+        names[1] = "Competitor 2";
+        names[2] = "Competitor 1";
+        names[3] = "Competitor 2";
+        names[4] = "Competitor 1";
+        StartDialogue();
+    }
+    //tier three lines
+    void thirdTierScript() {
+        lines = new string[10];
+        lines[0] = "Ah yes, we meet again.";
+        lines[1] = "That was supposed to be my line!";
+        lines[2] = "C said you can't have everything to yourself, Jav.";
+        lines[3] = "Which one of you two said that!?";
+        lines[4] = "Anyways challenger, We came here to tell you just how great of a job you are doing, even though you are stuck here basically forever.";
+        lines[5] = "Well, there may be a way out for you. Not that I would want you to find that out, because I enjoy watching you play this game.";
+        lines[6] = "Only Gary has ever escaped successfully. Most players struggle to escape.";
+        lines[7] = "Does the player even know who Gary is?";
+        lines[8] = "I have said too much. I won't be in trouble, will I?";
+        lines[9] = "Don't be upset. I'm just happy the player hasn't bombed the place yet. Wait I wasn't supposed to say the yet part. STOP TALKING!";
+       
+       names = new string[10];
+       names[0] = "C. Plusplus";
+        names[1] = "Jav Ascript";
+        names[2] = "C. Plusplus";
+        names[3] = "Jav Ascript";
+        names[4] = "C. Sharp";
+        names[5] = "Jav Ascript";
+        names[6] = "C. PlusPlus";
+        names[7] = "Jav Ascript";
+        names[8] = "C. PlusPlus";
+        names[9] = "Jav Ascript";
+        StartDialogue();
+    }
 }
